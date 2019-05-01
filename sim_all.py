ee9ee362158c189d0fa71b9367bfb0145fe11c70
Sebastian Nyholm <sebn89@gmail.com>
2019-05-01 11:04:30 +0200

    added pattern recognition

--- sim_all.py
+++ sim_all.py
@@ -17,7 +17,10 @@ def analyze(session_folder):
     session_path = "session_data/" + session_folder + "/"
     test_folder = session_path + "test_" + type_of_cal + "/"
     config_filename = session_path + "config.csv"
-    cal_filename = test_folder + "training_fixation.csv"
+#    cal_filename = test_folder + "training_fixation.csv"
+#    cal_filename = test_folder + "training_pursuit_circle.csv"
+#    cal_filename = test_folder + "training_pursuit_linear.csv"
+    cal_filename = test_folder + "training_pursuit_spiral.csv"
     
     print("")
     print("Computing analyze linear transformation")
@@ -25,21 +28,24 @@ def analyze(session_folder):
     
     analyzer = gda.GazeDataAnalyzer()
     print("\nSETUP TRANSFORMATION")
+#    analyzer.setup(config_filename, cal_filename, "dbscan_fixation")
     analyzer.setup(config_filename, cal_filename, "dbscan_pursuit")
 #    print("\nTRAINING DATA")
 #    analyzer.analyze(cal_filename, "dbscan_fixation")
     
     print("\nTEST DATA - FIXATION")
     training_filename = test_folder + "training_fixation.csv"
-    rmse_deg_raw, rmse_deg_cor, rmse_deg_imp = analyzer.analyze(training_filename, "dbscan_pursuit", "values")
-    
+    rmse_deg_raw, rmse_deg_cor, rmse_deg_imp = analyzer.analyze(training_filename, "dbscan_fixation", "values")
+
+    fixation_deg_raw.append(rmse_deg_raw)    
     fixation_deg_cor.append(rmse_deg_cor)
     fixation_deg_imp.append(rmse_deg_imp)
     
     print("\nTEST DATA - PURSUIT (CIRCLE)")
     training_filename = test_folder + "training_pursuit_circle.csv"
     rmse_deg_raw, rmse_deg_cor, rmse_deg_imp = analyzer.analyze(training_filename, "dbscan_pursuit", "values")
-    
+
+    pursuit_circle_deg_raw.append(rmse_deg_raw)    
     pursuit_circle_deg_cor.append(rmse_deg_cor)
     pursuit_circle_deg_imp.append(rmse_deg_imp)
     
@@ -47,6 +53,7 @@ def analyze(session_folder):
     training_filename = test_folder + "training_pursuit_linear.csv"
     rmse_deg_raw, rmse_deg_cor, rmse_deg_imp = analyzer.analyze(training_filename, "dbscan_pursuit", "values")
 
+    pursuit_linear_deg_raw.append(rmse_deg_raw)
     pursuit_linear_deg_cor.append(rmse_deg_cor)
     pursuit_linear_deg_imp.append(rmse_deg_imp)
 
@@ -54,6 +61,7 @@ def analyze(session_folder):
     training_filename = test_folder + "training_pursuit_spiral.csv"
     rmse_deg_raw, rmse_deg_cor, rmse_deg_imp = analyzer.analyze(training_filename, "dbscan_pursuit", "values")
 
+    pursuit_spiral_deg_raw.append(rmse_deg_raw)
     pursuit_spiral_deg_cor.append(rmse_deg_cor)
     pursuit_spiral_deg_imp.append(rmse_deg_imp)
 
@@ -135,16 +143,20 @@ def analyze(session_folder):
 print("Calibrating for default:")
 print("------------------------")
 # Run analyse on
-type_of_cal = "custom_5p"
+type_of_cal = "default"
 
 print("")
 
+fixation_deg_raw = []
 fixation_deg_cor = []
 fixation_deg_imp = []
+pursuit_circle_deg_raw = []
 pursuit_circle_deg_cor = []
 pursuit_circle_deg_imp = []
+pursuit_linear_deg_raw = []
 pursuit_linear_deg_cor = []
 pursuit_linear_deg_imp = []
+pursuit_spiral_deg_raw = []
 pursuit_spiral_deg_cor = []
 pursuit_spiral_deg_imp = []
 
@@ -156,21 +168,47 @@ analyze("ctrl_group_2_mikkel")
 analyze("ctrl_group_2_lukas")
 analyze("ctrl_group_2_seb")
 
-print("Fixations value")
-print(fixation_deg_imp)
-print(fixation_deg_cor)
+def print_nice(values):
+    my_str = ""
+    for i in range(len(values)):
+        my_str += str(values[i]) + "\t"
+    print(my_str)
 
-print("Pursuit circle value")
-print(pursuit_circle_deg_imp)
-print(pursuit_circle_deg_cor)
+print("Improvements values")
+print_nice(fixation_deg_imp)
+print_nice(pursuit_circle_deg_imp)
+print_nice(pursuit_linear_deg_imp)
+print_nice(pursuit_spiral_deg_imp)
 
-print("Pursuit linear value")
-print(pursuit_linear_deg_imp)
-print(pursuit_linear_deg_cor)
+print("Correction values")
+print_nice(fixation_deg_cor)
+print_nice(pursuit_circle_deg_cor)
+print_nice(pursuit_linear_deg_cor)
+print_nice(pursuit_spiral_deg_cor)
 
-print("Pursuit spiral value")
-print(pursuit_spiral_deg_imp)
-print(pursuit_spiral_deg_cor)
+print("Raw values")
+print_nice(fixation_deg_raw)
+print_nice(pursuit_circle_deg_raw)
+print_nice(pursuit_linear_deg_raw)
+print_nice(pursuit_spiral_deg_raw)
+
+
+#
+#print("Fixations value")
+#print(fixation_deg_imp)
+#print(fixation_deg_cor)
+#
+#print("Pursuit circle value")
+#print(pursuit_circle_deg_imp)
+#print(pursuit_circle_deg_cor)
+#
+#print("Pursuit linear value")
+#print(pursuit_linear_deg_imp)
+#print(pursuit_linear_deg_cor)
+#
+#print("Pursuit spiral value")
+#print(pursuit_spiral_deg_imp)
+#print(pursuit_spiral_deg_cor)
 
 
 print("")
